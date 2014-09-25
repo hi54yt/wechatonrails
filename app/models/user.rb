c00fd73285236bf90a7acf9d2f0b504931c7dd00
Lanvige Jiang <lanvige@gmail.com>
2014-09-25 22:08:07 +0800

    update gemfile

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,19 @@
 class User < ActiveRecord::Base
-  enum role: [:user, :vip, :admin]
   after_initialize :set_default_role, :if => :new_record?
+
+  enum role: [:user, :vip, :admin]
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :invitable, :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+
+
+
+  private
 
   def set_default_role
     self.role ||= :user
   end
-
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :invitable, :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
 end
